c67fc29b3d1011efae38e645b69be4cda681b7fa
julienperron <julien.perron@outlook.com>
2016-03-01 14:24:55 +0100

    Ajout des erreurs dans newsletter (null,email(regex) etc...)

--- newsletter.php
+++ newsletter.php
@@ -2,27 +2,41 @@
 	
 	include('includes/connexion.inc.php');
 
-	if(isset($_GET['email'])) //si il y a un get
+	if(isset($_GET['email'])) //si il y a un get, si l'mail n'est pas nul et si si c'est bien un format email
 	{
-		$email = $_GET['email'];
+		if($_GET['email'] != "")
+		{
+			if((preg_match('#^[\w.-]+@[\w.-]+\.[a-z]{2,6}$#i', $_GET['email'])))
+			{
+				$email = $_GET['email'];
 
-		$res = mysql_query("SELECT * FROM newsletter WHERE email = '".$email."'"); //on sélectionne via l'email
-		$data = mysql_fetch_array($res);
-		
-		if($data) //si il existe
-		{
-			echo "Déjà abonné !"; //on envoie cela
+				$res = mysql_query("SELECT * FROM newsletter WHERE email = '".$email."'"); //on sélectionne via l'email
+				$data = mysql_fetch_array($res);
+				
+				if($data) //si il existe
+				{
+					echo "Vous êtes déjà abonné !"; //on envoie cela
+				}
+				else//si il n'existe pas
+				{
+					$sql = "INSERT INTO newsletter VALUES ('', '".$email."');"; //on insert
+					mysql_query($sql);
+					echo "Vous êtes abonné !";
+				}
+			}
+			else
+			{
+				echo "Ceci n'est pas un email !";
+			}
 		}
-		else //si il n'existe pas
+		else
 		{
-			$sql = "INSERT INTO newsletter VALUES ('', '".$email."');"; //on insert
-			mysql_query($sql);
-			echo "OK";
+			echo "Champs vide !";
 		}
 	}
 	else //si il n'y a pas de get alors on envoie cela
 	{
-		echo "KO"; 
+		echo "Erreur ! Réessayer !"; 
 	}
 
 ?>
